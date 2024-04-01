c5818210c5622fa6ad19a7ff0abd4e2e4dc4818b
MusaabSaif <164238476+MusaabSaif@users.noreply.github.com>
2024-04-02 02:26:55 +0500

    BMI calculator

---
+++ BMI Calculator/first.js
@@ -0,0 +1,49 @@
+// accessing all the divs through their id's //
+
+const heightInput = document.getElementById("height");
+const weightInput = document.getElementById("weight"); 
+const result = document.getElementById("results");
+const calculateBtn = document.querySelector("button");
+const resetBtn = document.querySelector("button");
+
+// adding the event listeners //
+
+resetBtn.addEventListener ("click" , function(event) {
+    display.value = "";
+});
+
+calculateBtn.addEventListener ("click" , function(event){
+    event.preventDefault();
+
+    const height = parseFloat(heightInput.value) / 100;
+    const weight = parseFloat(weightInput.value);
+
+    if (!isNaN (height) && !isNaN (weight)) {
+        const bmi = calculateBMI(height,weight);
+        result.textContent = `BMI : ${bmi.toFixed(1)}`;
+
+        displayBMIResult (bmi);
+    } else {
+        result.textContent = "Invalid Input";
+    }
+});
+
+// adding functions to caculate the bmi
+
+function calculateBMI(height,weight) {
+    return weight / (height * height);
+}
+
+// adding function to display remarks according to your result
+
+function displayBMIResult (bmi) {
+    const weightGuide = document.getElementById("weight-guide");
+    if (bmi < 18.6) {
+        weightGuide.textContent = "Underweight";
+    }  else if (bmi > 18.6 && bmi <= 24.9) {
+        weightGuide.textContent = "Normal weight";
+    }  else {
+        weightGuide.textContent = "Overweight";
+    }
+};
+
